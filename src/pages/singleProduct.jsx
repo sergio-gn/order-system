0925705beb36161dd87fa062b5afcd8e93303ef2
Sergio Garcia Neto <88346250+sergio-gn@users.noreply.github.com>
2023-08-24 20:01:00 +1000

    css updates

--- src/pages/singleProduct.jsx
+++ src/pages/singleProduct.jsx
@@ -28,35 +28,41 @@ function SingleProduct() {
     <div className="container">
       {product ? (
       <div className="card-singleProduct">
-        <div className="d-center justify-center">
-          <div className="image-product-wrapper">
-            {product.photoUrl && <img className="product-photo" src={product.photoUrl} alt={product.name} />}
-          </div>
-        </div>
-        <div className="d-center justify-center flex-direction-column">
-          <h2>{product.name}</h2>
-          <p>{product.price}</p>
-          <p>{product.code}</p>
-          <p>{product.description}</p>
-          <QuantityInput
-            quantity={quantity}
-            onQuantityChange={(newQuantity) => setQuantity(newQuantity)}
-          />
-          <AddToCartButton
-            product={product}
-            quantity={quantity}
-            onClick={() => setClickedCart(true)}
-          />
-          {clickedCart  ?
-            <div className="modalAfterClick">
-              <Link to="/cart">
-                <button >Ir Para o Carrinho</button>
-              </Link>
-              <Link to="/">
-                <button >Continuar Comprando</button>
-              </Link>
+        <div className="d-center flex-direction-column">
+          <div className="d-flex justify-center">
+            <div className="">
+              <div className="image-product-wrapper">
+                {product.photoUrl && <img className="product-photo" src={product.photoUrl} alt={product.name} />}
+              </div>
+              <h2>{product.name}</h2>
+              <p>{product.code}</p>
             </div>
-          : ""}
+            <div className="">
+              <p>{product.price}</p>
+              <p>{product.description}</p>
+            </div>
+          </div>
+          <div className="singleProduct-actions">
+            <QuantityInput
+              quantity={quantity}
+              onQuantityChange={(newQuantity) => setQuantity(newQuantity)}
+            />
+            <AddToCartButton
+              product={product}
+              quantity={quantity}
+              onClick={() => setClickedCart(true)}
+            />
+            {clickedCart  ?
+              <div className="modalAfterClick">
+                <Link to="/cart">
+                  <button >Ir Para o Carrinho</button>
+                </Link>
+                <Link to="/">
+                  <button >Continuar Comprando</button>
+                </Link>
+              </div>
+            : ""}
+          </div>
         </div>
       </div>
       ) : (
