ab97725b425df67afcb99efc2c9654ed10e5ef73
Sergio Garcia Neto <88346250+sergio-gn@users.noreply.github.com>
2023-09-04 19:33:29 +1000

    button ok edit

--- src/components/ui/editButton.jsx
+++ src/components/ui/editButton.jsx
@@ -1,21 +1,20 @@
 import React, { useState } from "react";
 
 function EditButton({buttonText, onClick}) {
-  // const [isEditing, setIsEditing] = useState(false);
+  const [isEditing, setIsEditing] = useState(false);
 
   const handleClick = () => {
-    // setIsEditing((prevIsEditing) => !prevIsEditing);
+    setIsEditing((prevIsEditing) => !prevIsEditing);
     onClick();
   };
 
   return (
     <button
-      // className={`edit-btn ${isEditing ? "edit-btn-green" : ""}`}
-      className="edit-btn"
+      className={`edit-btn ${isEditing ? "edit-btn-green" : ""}`}
+      // className="edit-btn"
       onClick={handleClick}
     >
-      {buttonText}
-      {/* {isEditing ? "Ok" : `${buttonText}`} */}
+      {isEditing ? "Ok" : `${buttonText}`}
     </button>
   );
 }
